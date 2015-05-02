3f6e097e4e0b3cbcd95518e90899d9d1b35293ae
Natasari <penitasari93@gmail.com>
2015-05-02 20:30:01 +0000

    coba referensi baru

---
+++ baru/ssl.php
@@ -0,0 +1,13 @@
+<?php 
+echo $_POST["name"]; 
+$dn = array(
+    "countryName" => "UK",
+    "stateOrProvinceName" => "Somerset",
+    "localityName" => "Glastonbury",
+    "organizationName" => "The Brain Room Limited",
+    "organizationalUnitName" => "PHP Documentation Team",
+    "commonName" => "Wez Furlong",
+    "emailAddress" => "wez@example.com"
+);
+print_r($dn);
+?>
